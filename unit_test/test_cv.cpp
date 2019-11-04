7c9cddb2695c93e9734b060dda3876cbe9e152d0
cggos <cggos@outlook.com>
2019-11-04 10:31:08 +0800

    add Unit Test for optical_flow

--- unit_test/test_cv.cpp
+++ unit_test/test_cv.cpp
@@ -2,16 +2,21 @@
 // Created by cg on 9/16/19.
 //
 
+#include <fstream>
+
 #include <gtest/gtest.h>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/video/tracking.hpp>
 
 #include "cv/types.h"
 #include "cv/yimg.h"
 #include "cv/corner_detector.h"
 #include "cv/image_filtering.h"
+#include "cv/visual_tracking.h"
 
 TEST(cv, Size)
 {
@@ -84,7 +89,6 @@ TEST(CornerDetector, detect_features)
     cv::waitKey(1000);
 }
 
-
 TEST(ImageFiltering, gaussian_blur) {
     cv::Mat mat_src = cv::imread("../data/lena.bmp", cv::ImreadModes::IMREAD_GRAYSCALE);
 
@@ -110,5 +114,94 @@ TEST(ImageFiltering, gaussian_blur) {
     cv::imshow("GaussianBlur YImg", mat_dst_01);
     cv::imshow("GaussianBlur OCV", mat_dst_02);
     cv::imshow("PyrDown YImg", mat_dst_03);
-    cv::waitKey(0);
+    cv::waitKey(1000);
+}
+
+TEST(VisualTracking, optical_flow) {
+    cv::Mat mat_src_01 = cv::imread("../data/optical_flow_01.png", cv::ImreadModes::IMREAD_GRAYSCALE);
+    cv::Mat mat_src_02 = cv::imread("../data/optical_flow_02.png", cv::ImreadModes::IMREAD_GRAYSCALE);
+
+    std::vector<cv::KeyPoint> kp1;
+    cv::Ptr<cv::GFTTDetector> detector = cv::GFTTDetector::create(500, 0.01, 20); // maximum 500 keypoints
+    detector->detect(mat_src_01, kp1);
+
+    std::vector<cv::Point2f> cv_pt1;
+    std::vector<mynt::Point2f> mynt_pt1;
+    for (auto &kp: kp1) {
+        cv_pt1.push_back(kp.pt);
+        mynt_pt1.push_back(mynt::Point2f(kp.pt.x, kp.pt.y));
+    }
+
+    /// use opencv's multi-level pyramids lk optical flow
+    std::vector<cv::Mat> img1_pyramid, img2_pyramid;
+    cv::buildOpticalFlowPyramid(mat_src_01, img1_pyramid, cv::Size(15, 15), 3, true, cv::BORDER_REFLECT_101, cv::BORDER_CONSTANT, false);
+    cv::buildOpticalFlowPyramid(mat_src_02, img2_pyramid, cv::Size(15, 15), 3, true, cv::BORDER_REFLECT_101, cv::BORDER_CONSTANT, false);
+
+    std::vector<cv::Point2f> cv_pt2_multi;
+    std::vector<uchar> status_multi;
+    cv::calcOpticalFlowPyrLK(
+            img1_pyramid, img2_pyramid, cv_pt1, cv_pt2_multi, status_multi, cv::noArray(), cv::Size(15, 15), 3,
+            cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 0.01));
+
+    /// then test multi-level LK
+    // create pyramids
+    std::vector<cv::Mat> pyr1, pyr2; // image pyramids
+    cv::Mat tmp1, tmp2;
+#if 1
+    for (int i = 0; i < 4; i++) {
+        if(i == 0) {
+            pyr1.push_back(mat_src_01);
+            pyr2.push_back(mat_src_02);
+            continue;
+        }
+        cv::pyrDown(pyr1[i-1], tmp1, pyr1[i-1].size() / 2);
+        pyr1.push_back(tmp1);
+        cv::pyrDown(pyr2[i-1], tmp2, pyr2[i-1].size() / 2);
+        pyr2.push_back(tmp2);
+    }
+#else
+    double scale = 1.0;
+    for (int i = 0; i < 4; i++) {
+        cv::resize(mat_src_01, tmp1, cv::Size(mat_src_01.cols * scale, mat_src_01.rows * scale));
+        pyr1.push_back(tmp1);
+        cv::resize(mat_src_02, tmp2, cv::Size(mat_src_02.cols * scale, mat_src_02.rows * scale));
+        pyr2.push_back(tmp2);
+        scale *= 0.5;
+    }
+#endif
+    std::vector<mynt::Point2f> kp2_multi;
+    std::vector<unsigned char> success_multi;
+    mynt::OpticalFlowMultiLevel(pyr1, pyr2, mynt_pt1, kp2_multi, success_multi, 15, 30);
+
+    /// write
+    std::ofstream out_file("debug_optical_flow.txt");
+    out_file << "OpticalFlowMultiLevel <--> calcOpticalFlowPyrLK" << std::endl;
+    for(int i=0; i<status_multi.size();  ++i) {
+        out_file << kp2_multi[i] << ", " << (int)success_multi[i] << " <--> "
+                 << cv_pt2_multi[i] << ", " << (int)status_multi[i] << std::endl;
+    }
+    out_file.close();
+
+    /// draw
+    cv::Mat cv_img2_multi;
+    cv::cvtColor(mat_src_02, cv_img2_multi, CV_GRAY2BGR);
+    for (int i = 0; i < cv_pt2_multi.size(); i++) {
+        if (status_multi[i]) {
+            cv::circle(cv_img2_multi, cv_pt2_multi[i], 2, cv::Scalar(0, 250, 0), 2);
+            cv::line(cv_img2_multi, cv_pt1[i], cv_pt2_multi[i], cv::Scalar(0, 250, 0));
+        }
+    }
+
+    cv::Mat img2_multi;
+    cv::cvtColor(mat_src_02, img2_multi, CV_GRAY2BGR);
+    for (int i = 0; i < kp2_multi.size(); i++) {
+        if (success_multi[i]) {
+            cv::circle(img2_multi, cv::Point2f(kp2_multi[i].x, kp2_multi[i].y), 2, cv::Scalar(0, 250, 0), 2);
+            cv::line(img2_multi, kp1[i].pt, cv::Point2f(kp2_multi[i].x, kp2_multi[i].y), cv::Scalar(0, 250, 0));
+        }
+    }
+
+    cv::imshow("tracked by opencv lk multi", cv_img2_multi);
+    cv::imshow("tracked multi level", img2_multi);
+    while (cv::waitKey(0) != 27);
 }

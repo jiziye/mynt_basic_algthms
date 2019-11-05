d8b2c62ab070b2f0a68ba0733214058ce88b309e
cggos <cggos@outlook.com>
2019-11-05 13:46:36 +0800

    update fast lib

--- 3rdparty/fast/test/test.cpp
+++ 3rdparty/fast/test/test.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <iostream>
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <fast/fast.h>
 
 int main (int argc, char * argv[]) {
